dea35b697d8e70477a442e5c117504ce8f540034
Travis Adams <tadams792@gmail.com>
2025-06-04 09:20:44 -0400

    Bump to v0.3.5

--- metaminer/__init__.py
+++ metaminer/__init__.py
@@ -6,7 +6,7 @@ from .schema_builder import build_schema_from_questions
 from .config import Config, setup_logging
 from .datatype_inferrer import DataTypeInferrer, TypeSuggestion, infer_question_types
 
-__version__ = "0.3.5rc"
+__version__ = "0.3.5"
 
 __all__ = [
     "Inquiry",
